82e24455a745e001ffacb992c2dd3c842ae4ea0e
picoCTF <agarwalkeshav0211@gmail.com>
2025-08-22 11:22:53 +0530

    Add more files on 22/08

---
+++ Test/src/myprint.java
@@ -0,0 +1,9 @@
+public class myprint implements printables, showable{
+    public void print(){
+        System.out.println("Print method is implemented");
+    }
+
+    public void show(){
+        System.out.println("Show method is implemented");
+    }
+}
---
+++ Test/src/printables.java
@@ -0,0 +1,11 @@
+public interface printables {
+    void print();
+    private void my(){
+        System.out.println("This is my private method");
+    }
+
+    public static void mystatic(){
+        System.out.println("This is my static method");
+    }
+
+}
