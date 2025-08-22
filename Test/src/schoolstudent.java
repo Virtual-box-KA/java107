e4f31b54b277e36a619ee95a983543c0741ca28c
picoCTF <agarwalkeshav0211@gmail.com>
2025-08-22 12:25:46 +0530

    Add super keyword file

---
+++ Test/src/schoolstudent.java
@@ -0,0 +1,12 @@
+public class schoolstudent extends person {
+    public String course;
+
+    public schoolstudent(String address, String course) {
+        super(address);
+        this.course = course;
+    }
+
+    public void getCourse(){
+        System.out.println("Course : "+course);
+    }
+}
---
+++ Test/src/schoostudentmain.java
@@ -0,0 +1,9 @@
+public class schoostudentmain {
+    public static void main(String[] args) {
+        person p = new person("Gzb");
+        p.getAddress();
+        schoolstudent sc = new schoolstudent("Mumbai", "JAVA");
+        sc.getCourse();
+        sc.getAddress();
+    }
+}
