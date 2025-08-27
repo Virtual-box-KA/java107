ff7833919bc4dff4d055306c4f1dcd0d471a914e
picoCTF <agarwalkeshav0211@gmail.com>
2025-08-27 14:03:02 +0530

    Added code files that includes exception

---
+++ Test/src/someException.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class someException {
+
+    public static void main(String[] args) {
+        int x, y, z=0;
+        Scanner sc = new Scanner(System.in);
+        try{
+            x = sc.nextInt();
+            y = sc.nextInt();
+            z=x/y;
+            System.out.println("No exception");
+        }
+
+        catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+        catch (InputMismatchException e){
+            System.out.println(e.getMessage());
+        }
+
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        finally{
+            System.out.println("Result is "+z);
+        }
+    }
+}
+
